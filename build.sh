c0f8bcbd2a5bb0b82131d4a97b4d9f6e4a0a7ed1
LHK1337 <lu.hal2001@gmail.com>
2018-12-05 14:53:20 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -59,24 +59,17 @@ make libmemenv.a
 
 cd ..
 
-#export BOOST_INCLUDE_PATH=../../boost_1_58_0/build/include
-export BDB_INCLUDE_PATH=../../db-4.8.30.NC/build_unix/include
-export OPENSSL_INCLUDE_PATH=../../openssl/build/include
-#export BOOST_LIB_PATH=../../boost_1_58_0/build/lib
-export BDB_LIB_PATH=../../db-4.8.30.NC/build_unix/lib
-export OPENSSL_LIB_PATH=../../openssl/build/lib
+#export BOOST_INCLUDE_PATH=$basePath/boost_1_58_0/build/include
+export BDB_INCLUDE_PATH=$basePath/db-4.8.30.NC/build_unix/include
+export OPENSSL_INCLUDE_PATH=$basePath/openssl/build/include
+#export BOOST_LIB_PATH=$basePath/boost_1_58_0/build/lib
+export BDB_LIB_PATH=$basePath/db-4.8.30.NC/build_unix/lib
+export OPENSSL_LIB_PATH=$basePath/openssl/build/lib
 
 make -f makefile.unix
 strip jumpcoind
 
 cd ..
-
-#export BOOST_INCLUDE_PATH=../boost_1_58_0/build/include
-export BDB_INCLUDE_PATH=../db-4.8.30.NC/build_unix/include
-export OPENSSL_INCLUDE_PATH=../openssl/build/include
-#export BOOST_LIB_PATH=../boost_1_58_0/build/lib
-export BDB_LIB_PATH=../db-4.8.30.NC/build_unix/lib
-export OPENSSL_LIB_PATH=../openssl/build/lib
 
 qmake
 make
