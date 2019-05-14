bfff3af2354dbf44a6609093ad3c34f8bece8f7e
LHK1337 <25325997+LHK1337@users.noreply.github.com>
2019-05-14 20:49:04 +0200

    boost-1.58 dl and compile

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 sudo apt-get update
-sudo apt-get install gcc make git wget curl qt4-dev-tools libqt4-dev libcxxtools-dev libboost1.58-all-dev -y
+sudo apt-get install gcc make git wget curl qt4-dev-tools libqt4-dev libcxxtools-dev -y
 
 mkdir jumpcoin_builKit
 cd jumpcoin_builKit
 
 export basePath=$(pwd)
 
-#wget http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.gz
+wget http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.gz
 wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
 
-#tar -xf boost_1_58_0.tar.gz
+tar -xf boost_1_58_0.tar.gz
 tar -xf db-4.8.30.NC.tar.gz
 
 git clone https://github.com/openssl/openssl.git
@@ -38,15 +38,15 @@ make install
 cd ..
 cd ..
 
-#cd boost_1_58_0
+cd boost_1_58_0
 
-#./bootstrap.sh --prefix=build
-#./b2 install --prefix=build
+./bootstrap.sh --prefix=build
+./b2 install --prefix=build
 
-#cd ..
+cd ..
 
-#rm boost_1_58_0/build/include/boost/asio/ssl/impl/context.ipp
-#curl https://raw.githubusercontent.com/LHK1337/JumpCoin-Build-Kit/master/context.ipp > boost_1_58_0/build/include/boost/asio/ssl/impl/context.ipp
+rm boost_1_58_0/build/include/boost/asio/ssl/impl/context.ipp
+curl https://raw.githubusercontent.com/LHK1337/JumpCoin-Build-Kit/master/context.ipp > boost_1_58_0/build/include/boost/asio/ssl/impl/context.ipp
 
 rm jumpcoin/src/makefile.unix
 curl https://raw.githubusercontent.com/LHK1337/JumpCoin-Build-Kit/master/makefile.unix > jumpcoin/src/makefile.unix
@@ -60,10 +60,10 @@ make libmemenv.a
 
 cd ..
 
-#export BOOST_INCLUDE_PATH=$basePath/boost_1_58_0/build/include
+export BOOST_INCLUDE_PATH=$basePath/boost_1_58_0/build/include
 export BDB_INCLUDE_PATH=$basePath/db-4.8.30.NC/build_unix/include
 export OPENSSL_INCLUDE_PATH=$basePath/openssl/build/include
-#export BOOST_LIB_PATH=$basePath/boost_1_58_0/build/lib
+export BOOST_LIB_PATH=$basePath/boost_1_58_0/build/lib
 export BDB_LIB_PATH=$basePath/db-4.8.30.NC/build_unix/lib
 export OPENSSL_LIB_PATH=$basePath/openssl/build/lib
 
